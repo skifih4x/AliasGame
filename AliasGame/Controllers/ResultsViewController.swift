fca690c6e42209735891d59d73775ecb4890e106
artem <arte@kb.ri>
2022-07-31 16:23:07 +0300

    initcomit

--- AliasGame/Controllers/ResultsViewController.swift
+++ AliasGame/Controllers/ResultsViewController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    @IBOutlet var overAllResultLabel: UILabel!
+    
+    @IBOutlet var guessWordsLabel: UILabel!
+    @IBOutlet var missedWordsLabel: UILabel!
+    
+    @IBOutlet var jokeJsonLabel: UILabel!
+    
+    var overAllResults: String!
+    var guessWords: String!
+    var missWords: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//        overAllResultLabel.text = overAllResults
+//        guessWordsLabel.text = guessWords
+//        missedWordsLabel.text = missWords
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
