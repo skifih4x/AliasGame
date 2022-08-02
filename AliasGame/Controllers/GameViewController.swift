9ac34e3e0b9c7a5caffb9989ac23e457f22a5dab
artem <arte@kb.ri>
2022-08-02 19:15:58 +0300

    done project

--- AliasGame/Controllers/GameViewController.swift
+++ AliasGame/Controllers/GameViewController.swift
@@ -1,171 +1,200 @@
-////
-////  GameViewController.swift
-////  AliasGame
-////
-////  Created by Артем Орлов on 27.07.2022.
-////
 //
-//import UIKit
-//import AVFoundation
+//  GameViewController.swift
+//  AliasGame
 //
-//class GameViewController: UIViewController {
-//    
+//  Created by Артем Орлов on 27.07.2022.
 //
-//    var player: AVAudioPlayer?
-//    
-//    var timer = Timer()
-//    var totalTime = 60
-//    var currentScore = 0
-//    var totalScore = 0
-//    var currentWord = "Здесь долженно быть слово передаваемое с экрана карточек"
-//    
-//    @IBOutlet weak var totalTimeLabel: UILabel! 
-//    @IBOutlet weak var scoreLabel: UILabel!
-//    @IBOutlet weak var wordLabel: UILabel!
-//    
-//    @IBOutlet weak var cancelButton: UIButton!
-//    @IBOutlet weak var doneButton: UIButton!
-//    @IBOutlet weak var startGame: UIButton!
-//
-//    @IBOutlet weak var nextButton: UIButton!
-//
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//
-//        // Do any additional setup after loading the view. 
-//
-//        WordStore.shared.setWords(by: topic)
-//        showWord()
-//        // Do any additional setup after loading the view.
-//
-//    }
-//    
-//
-//  
-//    @IBAction func refreshButton(_ sender: UIButton) {
-//        //Обновляет раунд и сбрасывает время
-//        //    timer.invalidate()
-//    }
-//    
-//    @IBAction func startGame(_ sender: UIButton) {
-//        //кнопки cancel и done становяться активными и их альфа изменяется на 1
-//        //кнопка startGame становиться неактивной и ее альфа меняется на 0.5
-//        //запускает отсчет времени
-//        //по окончании отсчета по хорошему надо что то делать с очками ?
-//        startGame.alpha = 0.5
-//        wordLabel.text = currentWord
-//        
-//        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-//    }
-//    
-//    @IBAction func cancelButton(_ sender: UIButton) {
-//        currentScore -= 1
-//        scoreLabel.text = "Очки: " + String(currentScore)
-//        
-//        // как только закончилось время кнопка должна стать неактивной
-//        // изменяется ее альфа на 0.5
-//    }
-//    
-//    @IBAction func doneButton(_ sender: UIButton) {
-//        currentScore += 1
-//        scoreLabel.text = "Очки: " + String(currentScore)
-//        
-//        // как только закончилось время кнопка должнать стать неактивной
-//        // изменяется ее альфа на 0.5
-//    }
-//    
-//    @objc func updateTimer() {
-//        if totalTime != 0 {
-//            totalTime -= 1
-//            totalTimeLabel.text = String(totalTime)
-//            
-//        }
-//        else {
-//            timer.invalidate() //disactivate timer
-//            totalScore = currentScore
-//            performSegue(withIdentifier: "resultVC", sender: nil)
-//            
-//            //Work NetworkManager
-//
-//            
-//            currentScore = 0
-//            cancelButton.alpha = 0.5
-//            doneButton.alpha = 0.5
-//
-////            playSound()
-//        }
-//    }
-//    
-//    
-//
-//    var topic = "russian_words_nouns" {
-//        didSet {
-//            WordStore.shared.setWords(by: topic)
-//        }
-//    }
-////
-////    @IBAction func nextPressed() {
-////        wordLabel.text = ""
-////        showWord()
-////    }
-//        
-//    func showWord() {
-//        wordLabel.text = WordStore.shared.randomWord()
-//    }
-//}
-//
-////extension GameViewController: JokeManagerDelegate {
-////    func didGetJoke(jokeModel: JokeModel) {
-////        DispatchQueue.main.async {
-////            let alert = UIAlertController(title: jokeModel.text, message: jokeModel.answer, preferredStyle: .alert)
-////            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-////                self.navigationController?.popToRootViewController(animated: true)
-////            }))
-////            self.present(alert, animated: true)
-////        }
-////
-////    }
-////
-////    func didFailWithError(error: Error) {
-////        print(error.localizedDescription)
-////    }
-////}
-//
-//
-//    /*
-//
-//    // MARK: - Navigation
-//
-//   
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard segue.identifier == "resultVC" else { return }
-//        guard let destination = segue.destination as? ResultsViewController else { return }
-////        destination.totalScore = totalScore // на контроллере ResultsViewController в коде создать переменную totalScore (туда передаться общий результат)
-//        
-//    }
-//
-//    
-//    // Method wich do play wav source (раскомментировать если нужно)
-//        func playSound() {
-//            guard let url = Bundle.main.url(
-//                forResource: "alarm_sound",
-//                withExtension: "mp3") else { return }
-//            
-//            do {
-//                try AVAudioSession.sharedInstance().setCategory(
-//                    .playback, mode: .default)
-//                try AVAudioSession.sharedInstance().setActive(true)
-//                
-//                player = try AVAudioPlayer(
-//                    contentsOf: url,
-//                    fileTypeHint: AVFileType.mp3.rawValue)
-//                
-//                guard let player = player else { return }
-//                player.play()
-//                
-//                } catch let error { print(error.localizedDescription) }
-//        }
-//}
-//*/
+
+import UIKit
+import AVFoundation
+
+class GameViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+    
+    var timer = Timer()
+    var totalTime = 60
+    var currentScore = 0
+    var totalScore = 0
+    var wordCount = 0
+    var topic = "russian_words_nouns" {
+        didSet {
+            WordStore.shared.setWords(by: topic)
+        }
+    }
+    
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var startGame: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        WordStore.shared.setWords(by: topic)
+        showWord()
+        doneButton.isHidden = true
+        cancelButton.isHidden = true
+    }
+    
+    @IBAction func refreshButton(_ sender: UIButton) {
+        //Обновляет раунд и сбрасывает время
+        timer.invalidate()
+        totalTime = 60
+        totalTimeLabel.text = String(60)
+        currentScore = 0
+        resultLabel.text = String(0)
+        wordCount = 0
+        startGame.isHidden = false
+        doneButton.isHidden = true
+        cancelButton.isHidden = true
+        
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        
+        timer.invalidate()
+        //        startGame.alpha = 0.5
+        wordLabel.text = ""
+        showWord()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        doneButton.isHidden = false
+        cancelButton.isHidden = false
+        startGame.isHidden = true
+    }
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+        updateTimerButton()
+        
+        currentScore -= 1
+        
+        if currentScore < 0 {
+            currentScore = 0
+        }
+        resultLabel.text = String(currentScore)
+        
+        if wordCount > 10 {
+            performSegue(withIdentifier: "resultVC", sender: nil)
+        } else {
+            showWord()
+        }
+        playSound(nameSound: "bad")
+        //                didGetJoke()
+    }
+    
+    @IBAction func doneButton(_ sender: UIButton) {
+        bonusRound()
+        updateTimerButton()
+        currentScore += 1
+        resultLabel.text = String(currentScore)
+        
+        if wordCount > 10 {
+            performSegue(withIdentifier: "resultVC", sender: nil)
+        } else {
+            showWord()
+        }
+        
+        playSound(nameSound: "good")
+        //                didGetJoke()
+    }
+    
+    @objc func updateTimer() {
+        if totalTime != 0 {
+            totalTime -= 1
+            totalTimeLabel.text = String(totalTime)
+            
+        } else {
+            timer.invalidate() //disactivate timer
+            totalScore = currentScore
+            
+            currentScore = 0
+            doneButton.isHidden = true
+            cancelButton.isHidden = true
+        }
+    }
+    
+    func bonusRound() {
+        
+        let i = bonusWord()
+        
+        if i == 5 {
+            currentScore += 2
+        } else {
+            resultLabel.textColor = .black
+        }
+    }
+    
+    func bonusWord() -> Int {
+        let randomWords = Int.random(in: 1...10)
+        print(randomWords)
+        if randomWords == 5 {
+            wordLabel.textColor = .red
+        } else if randomWords != 5 {
+            wordLabel.textColor = .black
+        }
+        return randomWords
+    }
+    
+    func showWord() -> Int {
+        wordLabel.text = WordStore.shared.randomWord()
+        wordCount += 1
+        return wordCount
+    }
+    
+    func updateTimerButton() {
+        timer.invalidate()
+        totalTime = 61
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "resultVC" else { return }
+        guard let destination = segue.destination as? ResultsViewController else { return }
+        destination.overAllResults = resultLabel.text
+    }
+    
+    //MARK: - PlaySound
+    
+    func playSound(nameSound: String) {
+        guard let url = Bundle.main.url(
+            forResource: nameSound,
+            withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(
+                .playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(
+                contentsOf: url,
+                fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            player.play()
+            
+        } catch let error { print(error.localizedDescription) }
+    }
+}
+
+//MARK: - UIAlertController
+
+extension GameViewController {
+    
+    func didGetJoke() {
+        var headJoke = ""
+        var footerJoke = ""
+        NetworkManager.shared.fetchJoke { joke in
+            headJoke = joke.setup
+            footerJoke = joke.punchline
+            let alert = UIAlertController(title: headJoke, message: footerJoke, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .cancel)
+            alert.addAction(alertAction)
+            self.present(alert, animated: true)
+        }
+    }
+}
+
